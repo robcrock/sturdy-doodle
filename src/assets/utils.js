9c59579309a250caa85c95567e70a63e3698b832
Robert Crocker <robert@vizsimply.com>
2020-12-30 14:48:50 -0300

    Installed uuid and created the util.js file.

---
+++ src/assets/utils.js
@@ -0,0 +1,126 @@
+import { v4 as uuidv4 } from "uuid"
+
+export default function dashboardList() {
+  return [
+    {
+      name: "Total Watch Time",
+      author: "Bonnie Sanchez",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-01.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi1",
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Average Watch Time",
+      author: "Clarence Ward",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-02.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi2",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Total Video Views",
+      author: "Theresa Young",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-03.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Total Unique Views",
+      author: "Virginia Martin",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-04.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi4",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Average Views per Viewer",
+      author: "Jose Stewart",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-05.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi5",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Impressions CTR",
+      author: "Joseph Brooks",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-06.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi6",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Audience Retention",
+      author: "Amanda Washington",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-07.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi7",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Engagement",
+      author: "Gregory Collins",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-08.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi8",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Subscribers Gained",
+      author: "Katherine Hall",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-09.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi9",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Subscribers Lost",
+      author: "Victor Turner",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-10.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi10",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Estimated Revenue",
+      author: "Anne Campbell",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-11.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi11",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Monitized Playbacks",
+      author: "Brandon Foster",
+      image:
+        "https://raw.githubusercontent.com/robcrock/embedded-side-nav/main/src/images/img-12.png",
+      url:
+        "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi12",
+      id: uuidv4(),
+      active: false,
+    },
+  ]
+}
