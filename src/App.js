a984c512ea3313b9d877fcc9e23009e9089789dc
Robert Crocker <robert@vizsimply.com>
2020-12-30 17:17:38 -0300

    Fixed the Tableau JS API script tag and logged the currentDashboard.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useState } from "react"
+// Import the data
+import data from "./assets/utils"
+// Import the styles
 import "./App.css"
 
 function App() {
+  // State
+  const [dashboards, setDashboards] = useState(data())
+  const [currentDashboard, setCurrentDashboard] = useState(dashboards[0])
+
+  console.log(currentDashboard)
+
   return <div className="App"></div>
 }
 
